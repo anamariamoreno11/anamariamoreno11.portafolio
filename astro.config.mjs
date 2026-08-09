2fd737c50e35617ab46d0f0d740b684803488e97
anamariamoreno11 <amorenoso@unbosque.edu.co>
2026-08-09 17:00:23 -0500

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astronaut.github.io',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
